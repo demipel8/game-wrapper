56e95c5972df10a5c347c8dc527271f1dea591ee
demi <demipel8@gmail.com>
2015-11-09 00:58:11 +0100

    Loader module ready to test

--- src/js/modules/M_Loader.js
+++ src/js/modules/M_Loader.js
@@ -4,16 +4,36 @@
 import base from '../interfaces/I_Loader'
 import RL from '../../../libs/resource-loader.min'
 
-let loader;
+let loader = {};
+let loadedResources = [];
 
+/**
+ * Instantiates the resource-loader object
+ */
 function initialize() {
   loader = new RL();
 }
-
+ /**
+  * Returns a promise that will be resolved once all queued resources are loaded
+  **/
 function start() {
-  loader.load();
+  return new promise(
+    function( resolve, reject ) {
+      loader.load(function (loader, resources) {
+        loadedResources = resources;
+      });
+
+      loader.on( 'complete', resolve );
+      loader.on('error', reject );
+    }
+  )
 }
 
+/**
+ * Adds an element to the resource queue
+ * @param name
+ * @param url
+ */
 function genericLoad( name, url ) {
   loader.add( name, url );
 }
@@ -22,6 +42,7 @@ base.initialize = initialize;
 base.start = start;
 base.image = genericLoad;
 base.audio = genericLoad;
-base.loader = () => loader;
+base.loader = loader;
+base.resources = loadedResources;
 
 export default base;
