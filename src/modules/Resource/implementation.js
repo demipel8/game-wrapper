3bb2eab134d0515f0048f3526aec1c19eed0a5ad
demi <demipel8@gmail.com>
2015-12-05 02:33:59 +0100

    Resource refactored tested and documented

--- src/modules/Resource/implementation.js
+++ src/modules/Resource/implementation.js
@@ -1,16 +1,17 @@
 import base from './interface'
 
 export default function( data = {} ) {
-  let keys = Object.keys( base );
+  let resource = Object.assign( {}, base );
+  let keys = Object.keys( resource );
 
   keys.forEach( function( name ) {
     if( typeof data[ name ] === 'undefined' ) {
       throw new Error( `data not compatible with resource interface: ${name} is missing` );
     } else {
-      base[ name ] = data[ name ];
+      resource[ name ] = data[ name ];
     }
   });
 
-  return base;
+  return resource;
 };
 
